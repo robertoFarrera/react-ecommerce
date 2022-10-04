2a76058744a31c37f9a10deef2211fe100705e16
Robert Farrera <rogonzalez@apexsystems.com>
2022-10-03 20:16:52 -0500

    Home refactored

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,35 +1,42 @@
-import CategoriesMenu from '../../components/CategoriesMenu/CategoriesMenu';
+import CategoryItem from '../../components/CategoryItem/CategoryItem';
+import { CategoriesMenuContainer } from './CategoriesMenu.styles';
+
+const categories = [
+  {
+    id: 1,
+    title: 'hats',
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+  },
+  {
+    id: 2,
+    title: 'jackets',
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+  },
+  {
+    id: 3,
+    title: 'sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+  },
+  {
+    id: 4,
+    title: 'womens',
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+  },
+  {
+    id: 5,
+    title: 'mens',
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+  },
+];
 
 const Home = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
-  return <CategoriesMenu categories={categories} />;
+  return (
+    <CategoriesMenuContainer>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </CategoriesMenuContainer>
+  );
 };
 
 export default Home;
